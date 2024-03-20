e3c689a2d0cc144c2c6a83ca8e09418799ab58b9
SmartBob12345 <99526587+SmartBob12345@users.noreply.github.com>
2024-03-19 21:15:17 -0700

    Add files via upload

---
+++ room2.js
@@ -0,0 +1,27 @@
+function preload(){
+    room = loadImage("image2.jpg");
+}
+function setup(){
+    canvas = createCanvas(600, 400);
+    canvas.center();
+    document.getElementById("status").innerHTML = "Detecting Objects";
+    detector = ml5.objectDetector("cocossd", loaded)
+}
+function draw(){
+    image(room, 0, 0, 600, 400);
+}
+function loaded(){
+    console.log("the model is loaded");
+    detector.detect(room, gotResults)
+}
+function gotResults(error, result){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(result);
+    }
+}
+function back(){
+    window.location = "index.html";
+}
